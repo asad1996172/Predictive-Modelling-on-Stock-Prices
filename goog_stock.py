10e18486543387b883674dc330297b621ae08017
asad1996172 <asad007mahmood@gmail.com>
2019-09-18 15:11:18 -0500

    Ran more ML models

--- goog_stock.py
+++ goog_stock.py
@@ -11,83 +11,43 @@ Original file is located at
 #              using the Machine Learning algorithm called Support Vector Regression (SVR)
 #              & Linear Regression.
 
-#Import the libraries
+# Import the libraries
 import pandas as pd
-import numpy as np
-from sklearn.svm import SVR
-from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+import utils
+import numpy
 
-#Load the data
-#from google.colab import files # Use to load data on Google Colab
-#uploaded = files.upload() # Use to load data on Google Colab
-df = pd.read_csv('GOOG_30_days.csv')
-df.head(7)
 
-#Create the lists / X and Y data sets
-dates = []
-prices = []
+# Load the data
+# from google.colab import files # Use to load data on Google Colab
+# uploaded = files.upload() # Use to load data on Google Colab
 
-#Get the number of rows and columns in the data set
-df.shape
+def create_plot(dates, original_prices, ml_models_outputs):
+    plt.scatter(dates, original_prices, color='black', label='Data')
+    for model in ml_models_outputs.keys():
+        plt.plot(dates, (ml_models_outputs[model])[0], color=numpy.random.rand(3, ), label=model)
 
-#Get the last row of data (this will be the data that we test on)
-df.tail(1)
+    plt.xlabel('Days')
+    plt.ylabel('Price')
+    plt.title('Regression')
+    plt.legend()
+    # plt.savefig("Plot.png")
+    plt.show()
 
-#Get all of the data except for the last row
-df = df.head(len(df)-1)
-df
 
-#The new shape of the data
-df.shape
+def train_predict_plot(file_name, ml_model):
+    df = pd.read_csv(file_name)
+    # print (df.head())
 
-#Get all of the rows from the Date Column
-df_dates = df.loc[:, 'Date']
-#Get all of the rows from the Open Column
-df_open = df.loc[:, 'Open']
+    ml_models_outputs = {}
 
-#Create the independent data set X
-for date in df_dates:
-  dates.append( [int(date.split('-')[2])])
-  
-#Create the dependent data se 'y'
-for open_price in df_open:
-  prices.append(float(open_price))
+    dates, prices, test_date, test_price = utils.getData(df)
+    # utils.LSTM_model(dates, prices, test_date, df)
+    for model in ml_model:
+        method_to_call = getattr(utils, model)
+        ml_models_outputs[model] = method_to_call(dates, prices, test_date, df)
 
-#See what days were recorded
-print(dates)
+    create_plot(dates, prices, ml_models_outputs)
 
-def predict_prices(dates, prices, x):
-  
-  #Create the 3 Support Vector Regression models
-  svr_lin = SVR(kernel='linear', C= 1e3)
-  svr_poly= SVR(kernel='poly', C=1e3, degree=2)
-  svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
-  
-  #Train the SVR models 
-  svr_lin.fit(dates,prices)
-  svr_poly.fit(dates,prices)
-  svr_rbf.fit(dates,prices)
-  
-  #Create the Linear Regression model
-  lin_reg = LinearRegression()
-  #Train the Linear Regression model
-  lin_reg.fit(dates,prices)
-  
-  #Plot the models on a graph to see which has the best fit
-  plt.scatter(dates, prices, color='black', label='Data')
-  plt.plot(dates, svr_rbf.predict(dates), color='red', label='SVR RBF')
-  plt.plot(dates, svr_poly.predict(dates), color='blue', label='SVR Poly')
-  plt.plot(dates, svr_lin.predict(dates), color='green', label='SVR Linear')
-  plt.plot(dates, lin_reg.predict(dates), color='orange', label='Linear Reg')
-  plt.xlabel('Days')
-  plt.ylabel('Price')
-  plt.title('Regression')
-  plt.legend()
-  plt.savefig("Plot.png")
-  
-  return svr_rbf.predict(x)[0], svr_lin.predict(x)[0],svr_poly.predict(x)[0],lin_reg.predict(x)[0]
 
-#Predict the price of GOOG on day 28
-predicted_price = predict_prices(dates, prices, [[28]])
-print(predicted_price)
+train_predict_plot('GOOG_30_days.csv', ['LSTM_model', 'BR', 'KNN', 'DT'])
---
+++ utils.py
@@ -0,0 +1,186 @@
+from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn import tree
+from sklearn import linear_model
+from sklearn.neural_network import MLPRegressor
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+import numpy
+import math
+# setting a seed for reproducibility
+numpy.random.seed(10)
+
+
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset) - look_back):
+        a = dataset[i:(i + look_back), 0]
+        dataX.append(a)
+        dataY.append(dataset[i + look_back, 0])
+    return numpy.array(dataX), numpy.array(dataY)
+
+
+def getData(df):
+    # Create the lists / X and Y data sets
+    dates = []
+    prices = []
+
+    # Get the number of rows and columns in the data set
+    # df.shape
+
+    # Get the last row of data (this will be the data that we test on)
+    last_row = df.tail(1)
+
+    # Get all of the data except for the last row
+    df = df.head(len(df) - 1)
+    # df
+
+    # The new shape of the data
+    # df.shape
+
+    # Get all of the rows from the Date Column
+    df_dates = df.loc[:, 'Date']
+    # Get all of the rows from the Open Column
+    df_open = df.loc[:, 'Open']
+
+    # Create the independent data set X
+    for date in df_dates:
+        dates.append([int(date.split('-')[2])])
+
+    # Create the dependent data se 'y'
+    for open_price in df_open:
+        prices.append(float(open_price))
+
+    # See what days were recorded
+    last_date = int(((list(last_row['Date']))[0]).split('-')[2])
+    last_price = float((list(last_row['Open']))[0])
+    return dates, prices, last_date, last_price
+
+
+def SVR_linear(dates, prices, test_date, df):
+    svr_lin = SVR(kernel='linear', C=1e3)
+    svr_lin.fit(dates, prices)
+    decision_boundary = svr_lin.predict(dates)
+
+    prediction = svr_lin.predict([[test_date]])[0]
+
+    return (decision_boundary, prediction)
+
+
+def SVR_poly(dates, prices, test_date, df):
+    svr_poly = SVR(kernel='poly', C=1e3, degree=2)
+    svr_poly.fit(dates, prices)
+    decision_boundary = svr_poly.predict(dates)
+
+    prediction = svr_poly.predict([[test_date]])[0]
+
+    return (decision_boundary, prediction)
+
+
+def SVR_rbf(dates, prices, test_date, df):
+    svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+    svr_rbf.fit(dates, prices)
+    decision_boundary = svr_rbf.predict(dates)
+
+    prediction = svr_rbf.predict([[test_date]])[0]
+
+    return (decision_boundary, prediction)
+
+
+def linear_regression(dates, prices, test_date, df):
+    lin_reg = LinearRegression()
+    lin_reg.fit(dates, prices)
+    decision_boundary = lin_reg.predict(dates)
+
+    prediction = lin_reg.predict([[test_date]])[0]
+
+    return (decision_boundary, prediction)
+
+
+def random_forests(dates, prices, test_date, df):
+    rand_forst = RandomForestRegressor(n_estimators=10, random_state=0)
+    rand_forst.fit(dates, prices)
+    decision_boundary = rand_forst.predict(dates)
+
+    prediction = rand_forst.predict([[test_date]])[0]
+
+    return (decision_boundary, prediction)
+
+
+def KNN(dates, prices, test_date, df):
+    knn = KNeighborsRegressor(n_neighbors=2)
+    knn.fit(dates, prices)
+    decision_boundary = knn.predict(dates)
+
+    prediction = knn.predict([[test_date]])[0]
+
+    return (decision_boundary, prediction)
+
+
+def DT(dates, prices, test_date, df):
+    decision_trees = tree.DecisionTreeRegressor()
+    decision_trees.fit(dates, prices)
+    decision_boundary = decision_trees.predict(dates)
+
+    prediction = decision_trees.predict([[test_date]])[0]
+
+    return (decision_boundary, prediction)
+
+
+def BR(dates, prices, test_date, df):
+    br = linear_model.BayesianRidge()
+    br.fit(dates, prices)
+    decision_boundary = br.predict(dates)
+
+    prediction = br.predict([[test_date]])[0]
+
+    return (decision_boundary, prediction)
+
+
+def LSTM_model(dates, prices, test_date, df):
+    df.drop(df.columns.difference(['Date', 'Open']), 1, inplace=True)
+    df = df['Open']
+    dataset = df.values
+    dataset = dataset.reshape(-1, 1)
+    dataset = dataset.astype('float32')
+
+    # normalize the dataset
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    dataset = scaler.fit_transform(dataset)
+
+    # split into train and test sets
+    train_size = len(dataset) - 2
+    train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
+
+    # reshape into X=t and Y=t+1
+    look_back = 1
+    trainX, trainY = create_dataset(train, look_back)
+    testX, testY = create_dataset(test, look_back)
+
+    # reshape input to be [samples, time steps, features]
+    trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+    testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
+    # create and fit the LSTM network
+    model = Sequential()
+    model.add(LSTM(4, input_shape=(1, look_back)))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+
+    # make predictions
+    trainPredict = model.predict(trainX)
+    testPredict = model.predict(testX)
+    # invert predictions
+    trainPredict = scaler.inverse_transform(trainPredict)
+    trainY = scaler.inverse_transform([trainY])
+    testPredict = scaler.inverse_transform(testPredict)
+    testY = scaler.inverse_transform([testY])
+    # calculate root mean squared error
+    return trainPredict, testPredict
